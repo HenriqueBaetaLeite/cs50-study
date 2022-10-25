56220eac98278b1b8cd408362c030636c205594d
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2022-10-24 23:20:38 -0300

    agree update

--- week6/agree.py
+++ week6/agree.py
@@ -1,6 +1,10 @@
 s = input("Do you agree? ")
 
 # if s == "y" or s == "Y":
+
+# s = s.lower()
+# if s in ["y", "yes"]:
+
 if s.lower() in ["y", "yes"]:
   print("Agreed")
 elif s in ["n", "N", "No", "NO", "no"]:
