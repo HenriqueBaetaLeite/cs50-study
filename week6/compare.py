ffb78d4208affc0af0afbdbea0cf6964d9824efb
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2022-10-26 09:17:34 -0300

    swap

---
+++ week6/compare.py
@@ -0,0 +1,14 @@
+s = input("s: ")
+t = input("t: ")
+
+if s == t:
+  print("Same")
+else:
+  print("Different")
+
+t = s.capitalize()
+
+print(s)
+print(t)
+
+# Strings are immutable
---
+++ week6/swap.py
@@ -0,0 +1,9 @@
+x = 1
+y = 3
+
+print(f"x = {x}")
+print(f"y = {y}")
+x, y = y, x
+
+print(f"x = {x}")
+print(f"y = {y}")
