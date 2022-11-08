d8c15341ba325ab3ff696251bdf2f5dc05b54488
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2022-11-08 17:06:37 -0300

    update: method post

--- week9/app.py
+++ week9/app.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   # name = request.args.get("name", "world")
+  # the line above is the same result as below
   # if "name" in request.args:
   #   name = request.args["name"]
   # else:
   #   name = "world"
   return render_template("index.html")
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
   name = request.args.get("name", "world")
   return render_template("greet.html", name=name)
