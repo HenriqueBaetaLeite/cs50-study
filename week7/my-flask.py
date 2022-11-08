b9c258961e922a2d9de5e68074cc2e3aab7d2272
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2022-11-07 22:26:57 -0300

    update

--- week7/my-flask.py
+++ week7/my-flask.py
@@ -7,7 +7,11 @@ from pytz import timezone
 app = Flask(__name__)
 
 @app.route("/")
-
 def time():
   now = datetime.now(timezone('America/New_York'))
   return "The current date and time in Cambridge is {}".format(now)
+
+
+@app.route("/show/<number>")
+def show(number):
+  return "You passed in {}".format(number)
